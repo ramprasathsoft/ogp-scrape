0a9f62cc8f583040356add7938a704e6bc2aeaee
Ramprasath S <53031231+ramprasathsoft@users.noreply.github.com>
2019-07-19 00:44:05 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -7,14 +7,14 @@ var app = require('../app');
 chai.use(chaiHttp);
 chai.should();
 describe("OpenGraphParams", () => {
-    describe("POST /ogp/scrape", () => {
+    describe("POST /ogp/scrape", function () {
+        this.timeout(150000); //total test timeout 2.5 min
         // Test to get all OGP details
-        it("should get all OGP details from api", (done) => {
+        it("should get all OGP details from api without cache", (done) => {
             chai.request(app)
                 .post('/ogp/scrape')
+                .timeout(60000) //timeout for this call 1min
                 .send({ "URL": "https://www.npmjs.com/package/node-cache", "NEED_CACHE": "N" })
-                .expect(200)
-                .expect('Content-Type', /json/)
                 .end(function (err, res) {
                     if (err) done(err);
                     res.body.should.have.property('success', true);
@@ -26,12 +26,27 @@ describe("OpenGraphParams", () => {
         });
 
         // Test to get all OGP details
-        it("should get all OGP details from cache", (done) => {
+        it("should get all OGP details and set to cache", (done) => {
             chai.request(app)
                 .post('/ogp/scrape')
+                .timeout(60000) //timeout for this call 1min
                 .send({ "URL": "https://www.npmjs.com/package/node-cache", "NEED_CACHE": "Y" })
-                .expect(200)
-                .expect('Content-Type', /json/)
+                .end(function (err, res) {
+                    if (err) done(err);
+                    res.body.should.have.property('success', true);
+                    res.body.should.have.property('requestUrl', "https://www.npmjs.com/package/node-cache");
+                    res.body.should.have.property('data');
+                    res.body.data.should.have.property('ogTitle', 'node-cache');
+                    done();
+                });
+        });
+
+        // Test to get all OGP details
+        it("should get all OGP details get from cache", (done) => {
+            chai.request(app)
+                .post('/ogp/scrape')
+                .timeout(10000) //get response from cache max time 10sec
+                .send({ "URL": "https://www.npmjs.com/package/node-cache", "NEED_CACHE": "Y" })
                 .end(function (err, res) {
                     if (err) done(err);
                     res.body.should.have.property('success', true);
